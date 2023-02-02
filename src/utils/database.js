93e2ffe99a53b459fcda5b63b2de8642db083f2e
leonardo <macstore27@gmail.com>
2023-02-02 02:34:19 -0500

    ssl acepth

--- src/utils/database.js
+++ src/utils/database.js
@@ -1,15 +1,15 @@
 const { Sequelize } = require("sequelize");
-require('dotenv').config()
+require("dotenv").config();
 
 const db = new Sequelize({
-    database: process.env.DB_NAME,
-    username: process.env.DB_USER,
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    password: process.env.DB_PASSWORD,
-    dialect: "postgres",
-    logging: false
+  database: process.env.DB_NAME,
+  username: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  password: process.env.DB_PASSWORD,
+  dialect: "postgres",
+  logging: false,
+  dialectOptions: { ssl: { require: true, rejectUnauthorized: false } },
 });
-
 
 module.exports = db;
